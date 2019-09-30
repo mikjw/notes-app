9264f9edfaa947a58c9d8747a62b98e235e5ec30
Rafaela <rafalucy3@gmail.com>
2019-09-30 14:12:38 +0100

    Implement the first test

---
+++ note-model.js
@@ -0,0 +1,12 @@
+
+(function(exports) {
+  function Note(input) {
+    this._text = input;
+  };
+
+  Note.prototype.text = function() {
+    return this._text;
+  };
+
+  exports.Note = Note;
+})(this);
---
+++ note-tests.js
@@ -0,0 +1,16 @@
+(function(exports) {
+  var assert = {
+    isTrue: function(assertion) {
+      if (!assertion) {
+        throw new Error('Assertion failed: ' + assertion + " is not truthy");
+      }
+    }
+  };
+
+  function returnTextAsInputed(){
+    var note = new Note('hello');
+      assert.isTrue(note.text() === 'hello')
+  };
+
+  returnTextAsInputed();
+})(this);
